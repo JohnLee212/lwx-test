d71dea1d64457dffe748c3bee1ea158af9402497
lwx <lwx18848956826@163.com>
2019-12-10 17:51:19 +0800

    first commit

---
+++ spring_boot_rabbitmq/src/test/java/com/john/study/spring_boot_rabbitmq/SpringBootRabbitmqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.john.study.spring_boot_rabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRabbitmqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
